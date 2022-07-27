9d393b6ff6b11f425869a8ad3f9e4663329b6842
Yanis540 <yanistabellout4@gmail.com>
2022-07-28 00:12:08 +0100

    full auth

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import test 
 from graphene_django.views import GraphQLView
 from .schema import schema
+import logging
+logging.getLogger("graphql.execution.utils").setLevel(logging.CRITICAL)
 urlpatterns = [
     path('graphql',GraphQLView.as_view(graphiql=True,schema=schema))
 ]
