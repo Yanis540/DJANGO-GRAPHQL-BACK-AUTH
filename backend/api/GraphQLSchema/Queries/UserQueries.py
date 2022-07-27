1304a065a41500c20b223d23f92446fee90e7d02
Yanis540 <yanistabellout4@gmail.com>
2022-07-27 19:46:00 +0100

    UserQuery

---
+++ backend/api/GraphQLSchema/Queries/UserQueries.py
@@ -0,0 +1,24 @@
+from api.Types.types import UserType 
+from graphene import ObjectType,List,Field,ID
+from api.models import User
+from djongo.models.fields import ObjectId
+
+class QueryUser(ObjectType):
+    users=List(UserType)
+    user=Field(UserType,args={
+        'id':ID(required=True)
+    })
+    
+    #add the middlewares
+    def resolve_users(parent,info):
+        try: 
+            return User.objects.all()
+        except Exception as e : 
+            raise Exception(e)
+
+    def resolve_user(parent,info,id):
+        try: 
+            return User.objects.get(_id=ObjectId(id))
+        except Exception as e : 
+            raise Exception(e)
+    
--- backend/api/schema.py
+++ backend/api/schema.py
@@ -1,13 +1,10 @@
-from graphene import ObjectType,Schema,Field,String
+from graphene import ObjectType,Schema
 
-from .Types.types import UserType
-from djongo.models.fields import ObjectId
-from .models import User
-class RootQuery(ObjectType):
-    name=Field(UserType)
-    def resolve_name(parent,info):
-        user=User.objects.get(_id=ObjectId('620bac44906d196bcfb5b2cd'))
-        return user
+# from .Types.types import UserType
+# from .models import User
+from .GraphQLSchema.Queries.UserQueries import QueryUser
+class RootQuery(QueryUser,ObjectType):
+    pass
 
 
 schema=Schema(query=RootQuery)
